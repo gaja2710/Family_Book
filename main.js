2da152c8a49065923532d14cb69a8c7986a40b1f
gaja2710 <90480594+gaja2710@users.noreply.github.com>
2023-05-27 11:23:26 +1000

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,4 @@
-var images = ["https://i.postimg.cc/MGn9GJXw/family.jpg", "https://i.postimg.cc/qqyYvVbq/grandpa.jpg", "https://i.postimg.cc/wjMnFtMX/father.jpg", "https://i.postimg.cc/5ymDKL83/bro.jpg", "https://i.postimg.cc/JnL6wtrd/sister.jpg", "https://i.postimg.cc/bw5W5zSK/mother.jpg"];
-var names = ["Family Book", "Ranbir Singh", "Diljeet Singh", "Rocky Singh", "Alia Singh", "Soni Singh"];
+var names = ["Fmaily Book", "Ranbir Singh", "Diljeet Singh", "Rocky Singh", "Alia Singh", "Soni Singh"];
 var i = 0;
 function update() {
   i++;
@@ -9,10 +8,10 @@ function update() {
   }
 
   //Debug the code to store list of images in updatedImage. Use images[i]
-  var updatedImages = [images];
+  var updatedImage = images[i];
   //Debug the code to store list of names in updatedName. Use names[i]
-  var updatedNames = [names];
+  var updatedName = names[i];
 
-  document.getElementById("family_member_image").src = updatedImages;
-  document.getElementById("family_member_name").innerHTML = updatedNames;
+  document.getElementById("family_member_image").src = updatedImage;
+  document.getElementById("family_member_name").innerHTML = updatedName;
 }
